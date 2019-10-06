3b2eced28ce5aad4f64fd84b4852e79217e7eea9
tianbing <tbbd426@gmail.com>
2019-10-06 20:48:02 +0800

    save code

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -427,6 +427,14 @@ func (rf *Raft) switchToLeader() {
 	rf.stopElecTimer()
 }
 
+/*
+leader发出心跳包
+TODO 实现
+ */
+func (rf *Raft) heartBeat() {
+
+}
+
 //
 // the service or tester wants to create a Raft server. the ports
 // of all the Raft servers (including this one) are in peers[]. this
